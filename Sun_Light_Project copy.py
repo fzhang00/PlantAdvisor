c6f6a4c38f53a60826d872fb6ad384ec885d4192
NotJohnsonC <101054552+NotJohnsonC@users.noreply.github.com>
2022-04-03 10:17:03 -0400

    changed the time to hours

--- Sun_Light_Project copy.py
+++ Sun_Light_Project copy.py
@@ -7,21 +7,24 @@ with open('microbit-data-13-2022-09-54-23-0400.csv', 'r') as f:
 
 light = [float(i[1]) for i in data[2:-1]]
 time = [float(i[0]) for i in data[2:-1]]
+hour = []
 threshold = 200
 accumulated = 0
 
+for t in time:
+    hour.append(t/60/60)
 for i in  range(1, len(time)):
     if light[i]>threshold:
-        accumulated = accumulated + (time[i] - time[i-1])
+        accumulated = accumulated + (hour[i] - hour[i-1])
     else:
         pass
-pyplot.title('The Light That Changes Over Time 27-2022-09-41-28-0500')
-pyplot.xlabel('Time, (seconds)')
+pyplot.title('The Light That Changes Over Time')
+pyplot.xlabel('Time, (hours)')
 pyplot.ylabel("The Light")
 pyplot.xticks(rotation = 45)
-pyplot.plot(time, light)
+pyplot.plot(hour, light)
+print("time recorded","13-2022-09-54-23-0400".split("-"))
 print("accumulated", accumulated)
-
 pyplot.show()
 
 
